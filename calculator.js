dc8c0095dab0e1281f58023a22def5fc35f916ff
Yan <yaneliyahu83@gmail.com>
2023-03-18 00:21:55 +0200

    Added logic

--- calculator.js
+++ calculator.js
@@ -1,36 +1,87 @@
 const auditElement = document.querySelector(".audit");
-const resultElement = document.querySelector(".result");
+const OPERATORS = ["/", "*", "-", "+"];
 
 function addEventListeners() {
-    // const auditElement = document.querySelector(".audit")
     const inputButtonsElements = document.querySelectorAll(".buttons-section>div>button")
     inputButtonsElements.forEach(item => {
         item.addEventListener('click', () => {
-            auditElement.setAttribute = auditElement.value += item.innerText;
-            console.log(`${item}`)
+            auditElement.setAttribute = auditElement.value += concatAudit(item);
         })
     })
 }
 
+function concatAudit(buttonElement) {
+    if (isFirstCharIsOperator(buttonElement)) {
+        alert("Invalid format used");
+        return "";
+
+    } else if (isOperatorComeAfterOperator(buttonElement)) {
+        alert("Invalid format used");
+        return "";
+
+    } else if (isEqualSign(buttonElement)) {
+        return "";
+
+    } else if (isCharLimitExceeded()) {
+        alert("Can't enter more than 22 characters")
+        return ""
+    }
+    return buttonElement.innerText;
+}
+
+function isFirstCharIsOperator(buttonElement) {
+    return auditElement.value.length == 0 &&
+         OPERATORS.includes(buttonElement.innerText) &&
+          isNaN(buttonElement.innerText[0])
+}
+
+function isEqualSign(buttonElement) {
+    return buttonElement.innerText == "="
+}
+
+function isOperatorComeAfterOperator(buttonElement) {
+    return OPERATORS.includes(auditElement.value.slice(-1)) && OPERATORS.includes(buttonElement.innerText)
+}
+
+function isCharLimitExceeded() {
+    return auditElement.value.length == 22
+}
+
 function clearAudit() {
-    console.log("clearing audit")
     auditElement.value = "";
-    resultElement.value = "";
 }
 
 function deleteValue() {
-    console.log(`deleting value: ${auditElement.value.slice(-1)}`)
     auditElement.value = auditElement.value.slice(0, -1);
 }
 
 function getResult() {
-    let result = resultElement.value = evaluateExpression();
-    getResult.value = result;
+    let result  = evaluateExpression();
+    auditElement.value = result;
 }
 
 function evaluateExpression() {
-    expression = auditElement.value;
-    return eval(expression);
+    let expression = auditElement.value;
+    let isExpressionValid = isExpressionValidValidation(expression);
+    if (isExpressionValid) {
+        let result = eval(expression);
+        auditElement.value = result;
+        return result
+    }
+    return "";
+}
+
+function isExpressionValidValidation(expression) {
+    if (expression?.slice(-2) == "/0") {
+        alert("Cant divide by 0")
+        return
+    } else if (OPERATORS.includes(expression.slice(-1))) {
+        alert("Invalid format used")
+        return
+    } else if (expression == "") {
+        return
+    }
+    return true
 }
 
 addEventListeners();
